53759defdd56e8d9548ba0352576bf28afed2259
Michael Crump <mbcrump29@gmail.com>
2020-09-19 10:31:35 -0700

    added save functionality

--- journal/journal.py
+++ journal/journal.py
@@ -1,17 +1,26 @@
-## This is the journal module
+## This is the journal module #app # 4
 
 import os 
 
 def load(name):
     data = []
     filename = get_full_pathname(name)
-    print(str(filename))
     if os.path.exists(filename):
-        print("Inside our if...statement")
-        with open(filename) as fin:
+        print("Open file")
+        with open(filename, 'r') as fin:
             for entry in fin.readlines():
                 data.append(entry.rstrip())
     return data
+
+def save(name, journal_data):
+    filename = get_full_pathname(name)
+    print("Saving data")
+    with open(filename, 'w') as fout:
+            for entry in journal_data:
+                fout.write(entry + "\n")
+
+def add_entry(text, journal_data):
+    journal_data.append(text)
 
 def get_full_pathname(name):
     filename = os.path.abspath(os.path.join(".", name + ".txt"))
--- journal/program.py
+++ journal/program.py
@@ -2,12 +2,37 @@ import journal
 
 def main():
     print("enter main")
-    loadfile()
+    run_event_loop()
+    
 
-
-def loadfile():
+def run_event_loop():
+    print("What would you like to do next with your journal?")
+    cmd = 'EMPTY'
     journal_name = "default_journal"
     journal_data = journal.load(journal_name)
-    print(journal_data)
+
+    while cmd != 'x' and cmd:
+        cmd = input("Select your command: [A]dd, [L]ist, E[x]it")
+        cmd = cmd.lower().strip()
+
+        if cmd == 'l':
+            list_entries(journal_data)
+        elif cmd == 'a':
+            add_entry(journal_data)
+        elif cmd != 'x' and cmd:
+            print("Please enter a valid number. ")
+
+    print("completed")
+    journal.save(journal_name, journal_data)
+
+def list_entries(data):
+    entries = reversed(data)
+    for idx, entry in enumerate(entries):
+        print('* [{}] {}'.format(idx +1 , entry))
+
+def add_entry(data):
+    text = input("Type your entry and press return when finished. ")
+    journal.add_entry(text, data)
+
 
 main()
